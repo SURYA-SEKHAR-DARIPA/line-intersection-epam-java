13cdd8cecf492b031b209df057e48e7214bc517d
Surya <suryasekhardaripa60461@gmail.com>
2023-02-01 13:08:11 +0530

    done

--- src/main/java/com/epam/rd/autotasks/intersection/Line.java
+++ src/main/java/com/epam/rd/autotasks/intersection/Line.java
@@ -1,13 +1,22 @@
 package com.epam.rd.autotasks.intersection;
 
 public class Line {
+    int k,b;
 
     public Line(int k, int b) {
-
+        this.k=k;
+        this.b=b;
     }
 
     public Point intersection(Line other) {
-        throw new UnsupportedOperationException();
+        int x,y;
+        if(other.k==k){
+            return null;
+        }
+        x=(other.b-b)/(k-other.k);
+        y=k*x+b;
+        //throw new UnsupportedOperationException();
+        return new Point(x,y);
     }
 
 }
